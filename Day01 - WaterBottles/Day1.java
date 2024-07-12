2bea8e02381da1a6257d6c66b911f48e7b486ea4
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-12 22:12:38 +0530

    Update Day1.java

--- Day01 - WaterBottles/Day1.java
+++ Day01 - WaterBottles/Day1.java
@@ -1,15 +1,27 @@
-class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
+import java.util.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numBottles = sc.nextInt();
+        int numExchange = sc.nextInt();
+
+        int ans = numWaterBottles(numBottles, numExchange);
+        System.out.println(ans);
+    }
+
+    public static int numWaterBottles(int numBottles, int numExchange) {
         int drinkable = 0;
         int maxdrinkable = numBottles;
         int empty = 0;
 
-        while(numBottles>0){
-            drinkable = numBottles/numExchange;
+        while(numBottles > 0) {
+            drinkable = numBottles / numExchange;
             maxdrinkable =  maxdrinkable + drinkable;
-            empty = numBottles%numExchange;
-            numBottles = drinkable+empty;
-            if(drinkable==0){
+            empty = numBottles % numExchange;
+            numBottles = drinkable + empty;
+            if(drinkable == 0) {
                 break;
             }
         }
