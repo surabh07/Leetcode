be88272f8bcb401ddd5de8e6e591429b7e763010
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-14 21:03:45 +0530

    Create Day03.java

---
+++ Day03 - Reverse Integer(7)/Day03.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void main(String[] args){
+      int x = sc.nextInt();
+
+      int ans = reverse(x);
+      System.out.println(ans);
+    }
+    
+    public int reverse(int x) {
+
+        long revx = 0;
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        while (x!=0){  
+            revx = revx*10 + x%10;
+            x = x/10;
+        }  
+
+        if(revx<min || revx>max){
+            return 0;
+        }
+
+        return (int)revx;
+    }
+}
