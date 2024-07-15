68f112850612880acafc0a5fcea6acf5e954de31
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-15 21:16:36 +0530

    Update Day04.java

--- Day04 - Crawler Log Folder(1598)/Day04.java
+++ Day04 - Crawler Log Folder(1598)/Day04.java
@@ -1,1 +1,29 @@
+class Solution {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      String logs = sc.nextLine();
 
+      int ans = minOperations(logs);
+      System.out.println(ans);
+    }
+    public static int minOperations(String[] logs) {
+        int depth = 0;
+        int n = logs.length;
+
+        for(int i=0;i<n;i++){
+            if(logs[i].equals("./")){
+                continue;
+            }
+            else if(logs[i].equals("../")){
+                depth--;
+                if(depth<0){
+                    depth = 0;
+                }
+            }
+            else{
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
