e0c4ce1e8629326bcea0ab768693709dd62f5249
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-16 14:45:46 +0530

    Update Day05.java

--- Day05-Container With Most Water(11)/Day05.java
+++ Day05-Container With Most Water(11)/Day05.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+class Solution {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int[] height = new int[n];
+
+    for(int i=0;i<n;i++){
+      height[i] = sc.nextInt();
+    }
+
+    System.out.println(maxArea(height));
+  }
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
