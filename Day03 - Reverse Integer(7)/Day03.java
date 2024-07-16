6f2bf675e4befdce63769b7836e9c0a571f2f96b
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-16 14:47:08 +0530

    Update Day03.java

--- Day03 - Reverse Integer(7)/Day03.java
+++ Day03 - Reverse Integer(7)/Day03.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 class Solution {
     public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
       int x = sc.nextInt();
 
       int ans = reverse(x);
