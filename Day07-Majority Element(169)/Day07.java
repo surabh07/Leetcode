0766d3c6b83fa9d4059029072bb3d208689cba37
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-18 23:13:42 +0530

    Update Day07.java

--- Day07-Majority Element(169)/Day07.java
+++ Day07-Majority Element(169)/Day07.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap <Integer, Integer> hm = new HashMap<>();
+        int n = nums.length;
+        int ans = 0;
 
+        for(int i : nums){
+            if(hm.containsKey(i)){
+                int temp = hm.get(i);
+                hm.put(i, temp+1);
+            }
+            else{
+                hm.put(i, 1);
+            }
+        }
+        
+        for(int i : nums){
+            if(hm.get(i)>n/2)
+                ans = i;
+        }
+        return ans;
+    }
+}
