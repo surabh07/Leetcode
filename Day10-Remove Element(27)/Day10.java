9c9ef5712addeabcd392f883dcdd31ca940569e0
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-21 23:52:29 +0530

    Update Day10.java

--- Day10-Remove Element(27)/Day10.java
+++ Day10-Remove Element(27)/Day10.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int removeElement(int[] nums, int val) {
+       int idx = 0;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i] == val){
+            continue;
+        }
+        else{
+            nums[idx] = nums[i];
+            idx++;
+        }
+       }
+       return idx;
+    }
+}
