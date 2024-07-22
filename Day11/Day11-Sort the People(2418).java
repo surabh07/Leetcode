e12e6076e4aded8d27df830f5be3f676eae17c0d
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-22 15:43:30 +0530

    Update Day11-Sort the People(2418).java

--- Day11/Day11-Sort the People(2418).java
+++ Day11/Day11-Sort the People(2418).java
@@ -1,1 +1,36 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = heights.length;
 
+        HashMap <Integer, String> hm = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            hm.put(heights[i], names[i]);
+        }
+
+        Arrays.sort(heights);
+
+        reverse(heights);
+
+        for(int i=0;i<n;i++){
+            String temp = hm.get(heights[i]);
+            names[i] = temp;
+        }
+
+        return names;
+    }
+
+    public static void reverse(int[] heights){
+        int sp = 0;
+        int ep = heights.length-1;
+
+        while(sp<ep){
+            int temp = heights[sp];
+            heights[sp] = heights[ep];
+            heights[ep] = temp;
+
+            sp++;
+            ep--;
+        }
+    }
+}
