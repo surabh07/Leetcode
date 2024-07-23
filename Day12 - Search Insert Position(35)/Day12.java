ac2fda586bbe1e059b90bc08550e56f504109646
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-23 23:52:50 +0530

    Update Day12.java

--- Day12 - Search Insert Position(35)/Day12.java
+++ Day12 - Search Insert Position(35)/Day12.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int i = 0;
 
+        for(i=0;i<n;i++){
+            if(nums[i] == target){
+                return i;
+            }
+            else{
+                if(nums[i]>target){
+                    return i;
+                }
+            }
+        }
+        return i;
+    }
+}
