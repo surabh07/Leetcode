dc17d230e4801728fad58aaa6d7fc68b50895125
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-24 23:41:50 +0530

    Update Day13.java

--- Day13 - Length of Last Word(58)/Day13.java
+++ Day13 - Length of Last Word(58)/Day13.java
@@ -1,1 +1,25 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
 
+        int length = 0;
+
+        for(int i=s.length()-1;i>=0;i--){
+            char c = s.charAt(i);
+
+            if(c>=65 && c<=90){
+                length++;
+            }
+            else if(c>=97 && c<=122){
+                length++;
+            }
+            else if(c>=48 && c<=57){
+                length++;
+            }
+            else if(c == ' ' && length > 0){
+                break;
+            }
+        }
+
+        return length;
+    }
+}
