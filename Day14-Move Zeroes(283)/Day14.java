6f9010b55337492f8a18dd85518dcedc1f8ebf3b
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-07-25 23:30:15 +0530

    Update Day14.java

--- Day14-Move Zeroes(283)/Day14.java
+++ Day14-Move Zeroes(283)/Day14.java
@@ -1,1 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
 
+        int idx = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0){
+                continue;
+            }
+            else{
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+                idx++;
+            }
+        }
+    }
+}
