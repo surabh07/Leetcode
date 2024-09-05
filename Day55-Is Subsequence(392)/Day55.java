f0eaabfdec0718513e1e5ab910ded402a6dff883
surabh07 <93609147+surabh07@users.noreply.github.com>
2024-09-06 03:24:57 +0530

    Create Day55.java

---
+++ Day55-Is Subsequence(392)/Day55.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sp = 0;
+        int tp = 0;
+
+        while (sp < s.length() && tp < t.length()) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+
+        if(sp == s.length()){
+            return true;
+        }
+
+        return false;       
+    }
+}
